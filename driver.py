00ba798d428a8f2c20264bf1cf2227d3c9ea519c
Ludvig Larsson <ludvig19@icloud.com>
2023-08-29 16:02:06 +0200

    improvement: selecting best percentual match in api response for the hardcoded group priorities

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 
-import requests, os, signal, json
+import requests, os, signal
+from fuzzywuzzy import process
 from dotenv import load_dotenv
 
 def main():
@@ -19,42 +20,45 @@ def main():
     resp_json = response.json()
 
     groups = {item['name'].lower(): item['id'] for item in resp_json}
-    groups.update({'eh': 26109}) # 
-    priority = {
+    groups.update({'eh': 26109}) # test: group that is closed for sure
+    priorities = {
         'eh' : 0,
         'pendulum' : 1, 
-        'forsyde radar' : 2, 
+        'forsyderadar' : 2, 
         'atlas-hgtd' : 3, 
-        'embedded ai-noc' : 4, 
-        'automated driving' : 5, 
-        'mist seud' : 6,
+        'embeddedai-noc' : 4, 
+        'automateddriving' : 5, 
+        'mistseud' : 6,
         'das' : 7, 
-        'electric vehicle' : 8
+        'electricvehicle' : 8
     }
 
-    current_best_priority = len(priority)
+    current_best_priority = len(priorities)
     while current_best_priority > 1:
-        for name in priority:
-            # if current_best_priority == 1:
-            #     print("[X] Highest prio was successfully joined, exiting")
-            #     break
-            # elif priority[name] >= current_best_priority:
-            #     continue
+        for name in priorities:
+            if current_best_priority == 1:
+                print("[S] Highest prio was successfully joined, exiting")
+                break
+            elif priorities[name] >= current_best_priority:
+                continue
+
+            group, _ = process.extractOne(name, groups.keys())
 
             resp = requests.post(
-                url=f"{base_url}/groups/{groups[name]}/memberships",
+                url=f"{base_url}/groups/{groups[group]}/memberships",
                 headers=headers,
                 data = { 'user_id': 'self' }
             )
             jsn = resp.json()
 
             if resp.status_code == 200:
-                print(f"[X] {name} ({priority[name]}) -> Joined, find better prio group now")
-                current_best_priority = priority[name]
+                print(f"[S] {group} ({priorities[name]}) -> Joined, find better prio group now")
+                current_best_priority = priorities[name]
             elif resp.status_code == 403:
-                print(f"[!] {name} ({priority[name]}) -> Group not open yet")
+                print(f"[!] {group} ({priorities[name]}) -> Group not open yet")
             elif resp.status_code == 400:
-                print(f"[!] {name} ({priority[name]}) -> {jsn['errors']['group_id'][0]['message']}")
+                msg = jsn['errors']['group_id'][0]['message']
+                print(f"[!] {group} ({priorities[name]}) -> {msg}")
             else:
                 print(f"[!] Code is {resp.status_code}, {jsn}")
 
