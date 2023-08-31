e489695b5db9586608d970ff9414848b3e40ee4d
Ludvig Larsson <ludvig19@icloud.com>
2023-08-31 15:14:09 +0200

    simplification: changed dict to list

--- driver.py
+++ driver.py
@@ -20,26 +20,26 @@ def main():
     resp_json = response.json()
 
     groups = {item['name'].lower(): item['id'] for item in resp_json}
-    groups.update({'eh': 26109}) # test: group that is closed for sure
-    priorities = {
-        'eh' : 0,
-        'pendulum' : 1, 
-        'forsyderadar' : 2, 
-        'atlas-hgtd' : 3, 
-        'embeddedai-noc' : 4, 
-        'automateddriving' : 5, 
-        'mistseud' : 6,
-        'das' : 7, 
-        'electricvehicle' : 8
-    }
+    # groups.update({'eh': 26109}) # test: group that is closed for sure
+    priorities = [
+        # 'eh',
+        'pendulum', 
+        'forsyderadar', 
+        'atlas-hgtd', 
+        'embeddedai-noc', 
+        'automateddriving', 
+        'mistseud',
+        'das', 
+        'electricvehicle'
+    ]
 
     current_best_priority = len(priorities)
     while current_best_priority > 1:
-        for name in priorities:
-            if current_best_priority == 1:
+        for prio, name in enumerate(priorities):
+            if current_best_priority == 0:
                 print("[S] Highest prio was successfully joined, exiting")
                 break
-            elif priorities[name] >= current_best_priority:
+            elif prio >= current_best_priority:
                 continue
 
             group, _ = process.extractOne(name, groups.keys())
@@ -55,13 +55,13 @@ def main():
             jsn = resp.json()
 
             if resp.status_code == 200:
-                print(f"[S] {group} ({priorities[name]}) -> Joined, find better prio group now")
-                current_best_priority = priorities[name]
+                print(f"[S] {group} ({prio}) -> Joined, find better prio group now")
+                current_best_priority = prio
             elif resp.status_code == 403:
-                print(f"[!] {group} ({priorities[name]}) -> Group not open yet")
+                print(f"[!] {group} ({prio}) -> Group not open yet")
             elif resp.status_code == 400:
                 msg = jsn['errors']['group_id'][0]['message']
-                print(f"[!] {group} ({priorities[name]}) -> {msg}")
+                print(f"[!] {group} ({prio}) -> {msg}")
             else:
                 print(f"[!] Code is {resp.status_code}, {jsn}")
 
